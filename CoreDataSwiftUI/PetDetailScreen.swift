857cfc3dbcd05d34514f8de1b15a1997d2c85140
Art <artem_chernenko@ro.ru>
2022-04-17 19:20:03 +0300

    detailsView setup

--- CoreDataSwiftUI/PetDetailScreen.swift
+++ CoreDataSwiftUI/PetDetailScreen.swift
@@ -9,16 +9,32 @@ import SwiftUI
 
 struct PetDetailScreen: View {
     let pet: Pets
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     var body: some View {
-        HStack {
-            Image(pet.picture ?? "star.fill")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 50, height: 50)
-                .cornerRadius(25)
+        VStack {
+//            Image(pet.picture ?? "star.fill")
+//                .resizable()
+//                .scaledToFill()
+//                .frame(width: 50, height: 50)
+//                .cornerRadius(25)
+            Text("\(pet.type!)")
             Text("\(pet.name!)")
         }
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Image(pet.picture ?? "star.fill")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 50, height: 50)
+                    .cornerRadius(25)
+            }
+        }
+        .navigationBarBackButtonHidden(true)
         Spacer()
+        Button("Save") {
+            presentationMode.wrappedValue.dismiss()
+        }
+        .padding()
     }
 }
 
